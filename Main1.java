ac3fbf40f13fb83568e52dbe0c15e0eb623bebc9
vinitha <vinitha9610@gmail.com>
2023-09-03 10:53:26 +0530

    second ommit

---
+++ Main1.java
@@ -0,0 +1,11 @@
+public class Main1 {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+         System.out.println("Hello and we");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+       
+    }
+}
