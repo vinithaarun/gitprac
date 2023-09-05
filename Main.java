f34f4b7c73e62eada39418dadc284190ef6be40d
vinitha <vinitha9610@gmail.com>
2023-09-05 16:07:25 +0530

    final commit

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
+         System.out.println("Hello and we");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
        
